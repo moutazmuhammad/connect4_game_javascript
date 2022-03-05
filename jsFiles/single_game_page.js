8f054437a60fc357ecddd867ed43f558ec446360
moutazmuhammad <moutazmuhammad1997@gmail.com>
2022-03-05 19:16:51 +0200

    Add Start and Reset Functoins

--- jsFiles/single_game_page.js
+++ jsFiles/single_game_page.js
@@ -1,29 +1,55 @@
 const circles = document.querySelectorAll(".circle")
 const username = document.querySelector("#username")
+
 let usernameNum = 1
+let startGame = 0
 
 for (let i=0 ; i<circles.length ; i++){
     circles[i].onclick=()=>{
-        if (circles[i].classList.contains("empty")){
+        if (circles[i].classList.contains("empty") && startGame === 1){
             if (usernameNum === 1){
                 circles[i].classList.add("playerOne");
                 circles[i].classList.remove("empty")
                 usernameNum = 2;
                 username.innerHTML= `Player ${usernameNum} Turn`;
                 if(i>6){
+                    circles[i-7].classList.remove("circle");
                     circles[i-7].classList.add("empty");
                 }
                 
             }
+
             else if (usernameNum === 2){
                 circles[i].classList.add("playerTwo");
                 circles[i].classList.remove("empty");
                 usernameNum = 1;
                 username.innerHTML= `Player ${usernameNum} Turn`;
                 if(i>6){
+                    circles[i-7].classList.remove("circle");
                     circles[i-7].classList.add("empty");
                 }
             }
         }
+    }
+}
+
+
+function startGameFunction() {
+    startGame = 1
+}
+
+function resetGameFunction() {
+    startGame = 0
+    for(let c=0 ; c < circles.length ; c++){
+        circles[c].classList.add("circle");
+        circles[c].classList.remove("playerTwo");
+        circles[c].classList.remove("playerOne");
+    }
+    for (let lRow=circles.length-7 ; lRow < circles.length ; lRow++ ){
+        circles[lRow].classList.add("empty");
+        circles[lRow].classList.remove("playerTwo");
+        circles[lRow].classList.remove("playerOne");
+        usernameNum = 1
+        username.innerHTML= `Player 1 Turn`;
     }
 }
