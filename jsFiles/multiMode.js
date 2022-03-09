cf84f6e425ca3df859a8e5af404eb468efa82492
Moutaz <moutazmuhammad1997@gmail.com>
2022-03-09 22:36:05 +0200

    Add start game as fly div

--- jsFiles/multiMode.js
+++ jsFiles/multiMode.js
@@ -1,14 +1,17 @@
 const circles = document.querySelectorAll(".circle")
 const username = document.querySelector("#username")
+// let level = document.cookie.split(";")[0].split("=")[1];
 let playerOneScore = 0
 let playerTwoScore = 0
 let usernameNum = 1
 let startGame = 0
+let level = 'hard'
 
 let check_draw = 1; //flage to check if player won
 
 let winEl = document.querySelector(".win-msg")
 let drawEl = document.querySelector(".draw-msg")
+let startEl = document.querySelector(".start-msg")
 
 // This part to get 2D array of circles indexs
 let positioncircles = 0; // This var to know which index value we are taking
@@ -53,7 +56,92 @@ diagonalsWinPositios=[
     [13, 19, 25, 31, 37],
 ]
 
-function checkPlayerOneWin(){
+function checkPlayerOneWinEasy(){
+    for (let i = 0 ; i < 6 ; i++){ // check circles in rows
+        for(let j = 0; j < 4; j++){
+            if (
+            circles[circlesIndex[i][j]].classList.contains("playerOne") &&
+            circles[circlesIndex[i][j+1]].classList.contains("playerOne") &&
+            circles[circlesIndex[i][j+2]].classList.contains("playerOne") &&
+            circles[circlesIndex[i][j+3]].classList.contains("playerOne")
+            ) {
+                check_draw = 0
+                playerOneScore++;
+                circles[circlesIndex[i][j]].classList.add("win")
+                circles[circlesIndex[i][j+1]].classList.add("win")
+                circles[circlesIndex[i][j+2]].classList.add("win")
+                circles[circlesIndex[i][j+3]].classList.add("win")
+
+                winEl.classList.add("show") // show fly div of winner
+            }
+        }
+    }
+
+    for (let i = 0 ; i < 7 ; i++){ // check circles in columns
+        for(let j = 0; j < 3; j++){
+            if (
+            circles[circlesIndex[j][i]].classList.contains("playerOne") &&
+            circles[circlesIndex[j+1][i]].classList.contains("playerOne") &&
+            circles[circlesIndex[j+2][i]].classList.contains("playerOne") &&
+            circles[circlesIndex[j+3][i]].classList.contains("playerOne")
+            ) {
+                check_draw = 0
+                playerOneScore++;
+                circles[circlesIndex[j][i]].classList.add("win")
+                circles[circlesIndex[j+1][i]].classList.add("win")
+                circles[circlesIndex[j+2][i]].classList.add("win")
+                circles[circlesIndex[j+3][i]].classList.add("win")
+
+                winEl.classList.add("show")
+            }
+        }
+    }
+}
+
+
+function checkPlayerTwoWinEasy(){
+    for (let i = 0 ; i < 6 ; i++){ // check circles in rows
+        for(let j = 0; j < 4; j++){
+            if (
+            circles[circlesIndex[i][j]].classList.contains("playerTwo") &&
+            circles[circlesIndex[i][j+1]].classList.contains("playerTwo") &&
+            circles[circlesIndex[i][j+2]].classList.contains("playerTwo") &&
+            circles[circlesIndex[i][j+3]].classList.contains("playerTwo")
+            ) {
+                check_draw = 0
+                playerTwoScore++;
+                circles[circlesIndex[i][j]].classList.add("win")
+                circles[circlesIndex[i][j+1]].classList.add("win")
+                circles[circlesIndex[i][j+2]].classList.add("win")
+                circles[circlesIndex[i][j+3]].classList.add("win")
+
+                winEl.classList.add("show") 
+            }
+        }
+    }
+
+    for (let i = 0 ; i < 7 ; i++){ // check circles in columns
+        for(let j = 0; j < 3; j++){
+            if (
+            circles[circlesIndex[j][i]].classList.contains("playerTwo") &&
+            circles[circlesIndex[j+1][i]].classList.contains("playerTwo") &&
+            circles[circlesIndex[j+2][i]].classList.contains("playerTwo") &&
+            circles[circlesIndex[j+3][i]].classList.contains("playerTwo")
+            ) {
+                check_draw = 0
+                playerTwoScore++;
+                circles[circlesIndex[j][i]].classList.add("win")
+                circles[circlesIndex[j+1][i]].classList.add("win")
+                circles[circlesIndex[j+2][i]].classList.add("win")
+                circles[circlesIndex[j+3][i]].classList.add("win")
+
+                winEl.classList.add("show")
+            }
+        }
+    }
+}
+
+function checkPlayerOneWinHard(){
     for (let i = 0 ; i < 6 ; i++){ // check circles in rows
         for(let j = 0; j < 3; j++){
             if (
@@ -124,7 +212,7 @@ function checkPlayerOneWin(){
     }
 }
 
-function checkPlayerTwoWin(){
+function checkPlayerTwoWinHard(){
     for (let i = 0 ; i < 6 ; i++){ // check circles in rows
         for(let j = 0; j < 3; j++){
             if (
@@ -222,10 +310,15 @@ for (let i=0 ; i<circles.length ; i++){
                     circles[i-7].classList.add("empty");
                 }
             }
-
         }
-        checkPlayerOneWin();
-        checkPlayerTwoWin();
+
+        if(level == "easy"){
+            checkPlayerOneWinEasy();
+            checkPlayerTwoWinEasy();
+        }else if(level == "hard"){
+            checkPlayerOneWinHard();
+            checkPlayerTwoWinHard();
+        }
         checkDraw();
     }
 }
@@ -233,6 +326,7 @@ for (let i=0 ; i<circles.length ; i++){
 
 function startGameFunction() {
     startGame = 1
+    startEl.classList.add("disappear")
 }
 
 function resetGameFunction() {
