7e02c6d8b332faeb1b52894aec81ac278e8a0cf6
4mohamedalaa <mohamed.elbatanouny28@gmail.com>
2022-03-10 18:07:34 +0200

    login modification

---
+++ jsFiles/multiMode.js
@@ -0,0 +1,350 @@
+const circles = document.querySelectorAll(".circle")
+const username = document.querySelector("#username")
+let level = document.cookie.split(";")[0].split("=")[1];
+let playerOneScore = 0
+let playerTwoScore = 0
+let usernameNum = 1
+let startGame = 0
+
+let check_draw = 1; //flage to check if player won
+
+let winEl = document.querySelector(".win-msg")
+let drawEl = document.querySelector(".draw-msg")
+let startEl = document.querySelector(".start-msg")
+
+// This part to get 2D array of circles indexs
+let positioncircles = 0; // This var to know which index value we are taking
+let circlesIndex = new Array(6);
+for (let t = 0; t < 6; t++) {
+    circlesIndex[t] = new Array(7);
+}
+
+for (let i = 0; i < 6; i++) {
+  for (let j = 0; j < 7; j++) {
+    circlesIndex[i][j] = i*7+j; 
+  }
+  positioncircles = positioncircles + 1; 
+}
+
+function checkDraw(){
+    let countOfPlayedCercles = 0; //flage to check fill of circles
+    for (let i = 0; i < 42; i++){
+        if((circles[i].classList.contains("playerOne")|| circles[i].classList.contains("playerTwo")))
+        {
+            countOfPlayedCercles = countOfPlayedCercles + 1;
+        } 
+        
+    }
+    if (countOfPlayedCercles === 42 && check_draw === 1){
+        drawEl.classList.add("show")
+    }
+}
+
+diagonalsWinPositios=[
+    [7, 15, 23, 31, 39],
+    [0, 8, 10, 24, 32],
+    [8, 10, 24, 32, 40],
+    [1, 9, 17, 25, 33],
+    [9, 17, 25, 33, 41],
+    [2, 10, 18, 26, 34],
+    [4, 10, 16, 22, 28],
+    [5, 11, 17, 23, 29],
+    [11, 17, 23, 29, 35],
+    [6, 12, 18, 24, 30],
+    [12, 18, 24, 30, 36],
+    [13, 19, 25, 31, 37],
+]
+
+function checkPlayerOneWinEasy(){
+    for (let i = 0 ; i < 6 ; i++){ // check circles in rows
+        for(let j = 0; j < 4; j++){
+            if (
+            circles[circlesIndex[i][j]].classList.contains("playerOne") &&
+            circles[circlesIndex[i][j+1]].classList.contains("playerOne") &&
+            circles[circlesIndex[i][j+2]].classList.contains("playerOne") &&
+            circles[circlesIndex[i][j+3]].classList.contains("playerOne")
+            ) {
+                check_draw = 0
+                playerOneScore++;
+                circles[circlesIndex[i][j]].classList.add("win")
+                circles[circlesIndex[i][j+1]].classList.add("win")
+                circles[circlesIndex[i][j+2]].classList.add("win")
+                circles[circlesIndex[i][j+3]].classList.add("win")
+
+                winEl.classList.add("show") // show fly div of winner
+            }
+        }
+    }
+
+    for (let i = 0 ; i < 7 ; i++){ // check circles in columns
+        for(let j = 0; j < 3; j++){
+            if (
+            circles[circlesIndex[j][i]].classList.contains("playerOne") &&
+            circles[circlesIndex[j+1][i]].classList.contains("playerOne") &&
+            circles[circlesIndex[j+2][i]].classList.contains("playerOne") &&
+            circles[circlesIndex[j+3][i]].classList.contains("playerOne")
+            ) {
+                check_draw = 0
+                playerOneScore++;
+                circles[circlesIndex[j][i]].classList.add("win")
+                circles[circlesIndex[j+1][i]].classList.add("win")
+                circles[circlesIndex[j+2][i]].classList.add("win")
+                circles[circlesIndex[j+3][i]].classList.add("win")
+
+                winEl.classList.add("show")
+            }
+        }
+    }
+}
+
+
+function checkPlayerTwoWinEasy(){
+    for (let i = 0 ; i < 6 ; i++){ // check circles in rows
+        for(let j = 0; j < 4; j++){
+            if (
+            circles[circlesIndex[i][j]].classList.contains("playerTwo") &&
+            circles[circlesIndex[i][j+1]].classList.contains("playerTwo") &&
+            circles[circlesIndex[i][j+2]].classList.contains("playerTwo") &&
+            circles[circlesIndex[i][j+3]].classList.contains("playerTwo")
+            ) {
+                check_draw = 0
+                playerTwoScore++;
+                circles[circlesIndex[i][j]].classList.add("win")
+                circles[circlesIndex[i][j+1]].classList.add("win")
+                circles[circlesIndex[i][j+2]].classList.add("win")
+                circles[circlesIndex[i][j+3]].classList.add("win")
+
+                winEl.classList.add("show") 
+            }
+        }
+    }
+
+    for (let i = 0 ; i < 7 ; i++){ // check circles in columns
+        for(let j = 0; j < 3; j++){
+            if (
+            circles[circlesIndex[j][i]].classList.contains("playerTwo") &&
+            circles[circlesIndex[j+1][i]].classList.contains("playerTwo") &&
+            circles[circlesIndex[j+2][i]].classList.contains("playerTwo") &&
+            circles[circlesIndex[j+3][i]].classList.contains("playerTwo")
+            ) {
+                check_draw = 0
+                playerTwoScore++;
+                circles[circlesIndex[j][i]].classList.add("win")
+                circles[circlesIndex[j+1][i]].classList.add("win")
+                circles[circlesIndex[j+2][i]].classList.add("win")
+                circles[circlesIndex[j+3][i]].classList.add("win")
+
+                winEl.classList.add("show")
+            }
+        }
+    }
+}
+
+function checkPlayerOneWinHard(){
+    for (let i = 0 ; i < 6 ; i++){ // check circles in rows
+        for(let j = 0; j < 3; j++){
+            if (
+            circles[circlesIndex[i][j]].classList.contains("playerOne") &&
+            circles[circlesIndex[i][j+1]].classList.contains("playerOne") &&
+            circles[circlesIndex[i][j+2]].classList.contains("playerOne") &&
+            circles[circlesIndex[i][j+3]].classList.contains("playerOne") &&
+            circles[circlesIndex[i][j+4]].classList.contains("playerOne")
+            ) {
+                check_draw = 0
+                playerOneScore++;
+                circles[circlesIndex[i][j]].classList.add("win")
+                circles[circlesIndex[i][j+1]].classList.add("win")
+                circles[circlesIndex[i][j+2]].classList.add("win")
+                circles[circlesIndex[i][j+3]].classList.add("win")
+                circles[circlesIndex[i][j+4]].classList.add("win")
+
+                winEl.classList.add("show") // show fly div of winner
+            }
+        }
+    }
+
+    for (let i = 0 ; i < 7 ; i++){ // check circles in columns
+        for(let j = 0; j < 2; j++){
+            if (
+            circles[circlesIndex[j][i]].classList.contains("playerOne") &&
+            circles[circlesIndex[j+1][i]].classList.contains("playerOne") &&
+            circles[circlesIndex[j+2][i]].classList.contains("playerOne") &&
+            circles[circlesIndex[j+3][i]].classList.contains("playerOne") &&
+            circles[circlesIndex[j+4][i]].classList.contains("playerOne")
+            ) {
+                check_draw = 0
+                playerOneScore++;
+                circles[circlesIndex[j][i]].classList.add("win")
+                circles[circlesIndex[j+1][i]].classList.add("win")
+                circles[circlesIndex[j+2][i]].classList.add("win")
+                circles[circlesIndex[j+3][i]].classList.add("win")
+                circles[circlesIndex[j+4][i]].classList.add("win")
+
+                winEl.classList.add("show")
+            }
+        }
+    }
+
+    for(let i= 0; i < diagonalsWinPositios.length ; i++){ // check circles in diagonals
+        const circle1 = circles[diagonalsWinPositios[i][0]]
+        const circle2 = circles[diagonalsWinPositios[i][1]]
+        const circle3 = circles[diagonalsWinPositios[i][2]]
+        const circle4 = circles[diagonalsWinPositios[i][3]]
+        const circle5 = circles[diagonalsWinPositios[i][4]]
+
+        if (circle1.classList.contains("playerOne") &&
+            circle2.classList.contains("playerOne") &&
+            circle3.classList.contains("playerOne") &&
+            circle4.classList.contains("playerOne") &&
+            circle5.classList.contains("playerOne")) 
+        {
+            playerOneScore++;
+            check_draw = 0
+            circles[diagonalsWinPositios[i][0]].classList.add("win")
+            circles[diagonalsWinPositios[i][1]].classList.add("win")
+            circles[diagonalsWinPositios[i][2]].classList.add("win")
+            circles[diagonalsWinPositios[i][3]].classList.add("win")
+            circles[diagonalsWinPositios[i][4]].classList.add("win")
+            
+            winEl.classList.add("show")
+        }
+    }
+}
+
+function checkPlayerTwoWinHard(){
+    for (let i = 0 ; i < 6 ; i++){ // check circles in rows
+        for(let j = 0; j < 3; j++){
+            if (
+            circles[circlesIndex[i][j]].classList.contains("playerTwo") &&
+            circles[circlesIndex[i][j+1]].classList.contains("playerTwo") &&
+            circles[circlesIndex[i][j+2]].classList.contains("playerTwo") &&
+            circles[circlesIndex[i][j+3]].classList.contains("playerTwo") &&
+            circles[circlesIndex[i][j+4]].classList.contains("playerTwo")
+            ) {
+                check_draw = 0
+                playerTwoScore++;
+                circles[circlesIndex[i][j]].classList.add("win")
+                circles[circlesIndex[i][j+1]].classList.add("win")
+                circles[circlesIndex[i][j+2]].classList.add("win")
+                circles[circlesIndex[i][j+3]].classList.add("win")
+                circles[circlesIndex[i][j+4]].classList.add("win")
+
+                winEl.classList.add("show") 
+            }
+        }
+    }
+
+    for (let i = 0 ; i < 7 ; i++){ // check circles in columns
+        for(let j = 0; j < 2; j++){
+            if (
+            circles[circlesIndex[j][i]].classList.contains("playerTwo") &&
+            circles[circlesIndex[j+1][i]].classList.contains("playerTwo") &&
+            circles[circlesIndex[j+2][i]].classList.contains("playerTwo") &&
+            circles[circlesIndex[j+3][i]].classList.contains("playerTwo") &&
+            circles[circlesIndex[j+4][i]].classList.contains("playerTwo")
+            ) {
+                check_draw = 0
+                playerTwoScore++;
+                circles[circlesIndex[j][i]].classList.add("win")
+                circles[circlesIndex[j+1][i]].classList.add("win")
+                circles[circlesIndex[j+2][i]].classList.add("win")
+                circles[circlesIndex[j+3][i]].classList.add("win")
+                circles[circlesIndex[j+4][i]].classList.add("win")
+
+                winEl.classList.add("show")
+            }
+        }
+    }
+
+    for(let i= 0; i < diagonalsWinPositios.length ; i++){ // check circles in diagonals
+        const circle1 = circles[diagonalsWinPositios[i][0]]
+        const circle2 = circles[diagonalsWinPositios[i][1]]
+        const circle3 = circles[diagonalsWinPositios[i][2]]
+        const circle4 = circles[diagonalsWinPositios[i][3]]
+        const circle5 = circles[diagonalsWinPositios[i][4]]
+
+        if (circle1.classList.contains("playerTwo") &&
+            circle2.classList.contains("playerTwo") &&
+            circle3.classList.contains("playerTwo") &&
+            circle4.classList.contains("playerTwo") &&
+            circle5.classList.contains("playerTwo")) 
+        {
+            playerTwoScore++;
+            check_draw = 0
+            circles[diagonalsWinPositios[i][0]].classList.add("win")
+            circles[diagonalsWinPositios[i][1]].classList.add("win")
+            circles[diagonalsWinPositios[i][2]].classList.add("win")
+            circles[diagonalsWinPositios[i][3]].classList.add("win")
+            circles[diagonalsWinPositios[i][4]].classList.add("win")
+            
+            winEl.classList.add("show")
+        }
+    }
+}
+
+// This Loop prepares the player's turn and the empty places to play
+for (let i=0 ; i<circles.length ; i++){
+
+    circles[i].onclick=()=>{
+
+        if (circles[i].classList.contains("empty") && startGame === 1){
+            if (usernameNum === 1){
+                circles[i].classList.add("playerOne");
+                circles[i].classList.remove("empty")
+                usernameNum = 2;
+                username.innerHTML= `Player ${usernameNum} Turn`;
+                if(i>6){
+                    circles[i-7].classList.remove("circle");
+                    circles[i-7].classList.add("empty");
+                }
+            }
+
+            else if (usernameNum === 2){
+                circles[i].classList.add("playerTwo");
+                circles[i].classList.remove("empty");
+                usernameNum = 1;
+                username.innerHTML= `Player ${usernameNum} Turn`;
+                if(i>6){
+                    circles[i-7].classList.remove("circle");
+                    circles[i-7].classList.add("empty");
+                }
+            }
+        }
+
+        if(level == "easy"){
+            checkPlayerOneWinEasy();
+            checkPlayerTwoWinEasy();
+        }else if(level == "hard"){
+            checkPlayerOneWinHard();
+            checkPlayerTwoWinHard();
+        }
+        checkDraw();
+    }
+}
+
+
+function startGameFunction() {
+    startGame = 1
+    startEl.classList.add("disappear")
+}
+
+function resetGameFunction() {
+    startGame = 0
+    check_draw = 0
+    
+    for(let i=0 ; i < 6 ; i++){
+        for (let j=0 ; j< 7 ; j++){
+            circles[circlesIndex[i][j]].classList.add("circle");
+            circles[circlesIndex[i][j]].classList.remove("playerTwo");
+            circles[circlesIndex[i][j]].classList.remove("playerOne");
+            circles[circlesIndex[i][j]].classList.remove("win");
+        }
+    }
+    for (let i=circles.length-7 ; i < circles.length ; i++ ){
+        circles[i].classList.add("empty");
+    }
+    winEl.classList.remove("show");
+    drawEl.classList.remove("show");
+    usernameNum = 1;
+    username.innerHTML= `Player 1 Turn`;
+}
--- jsFiles/singleMode.js
+++ jsFiles/singleMode.js
@@ -6,6 +6,10 @@ let resetBtn = document.getElementById("reset-button");
 let gameFrame = document.getElementsByClassName("gameframe")[0];
 let gameStarted = false;
 let turn = "player";
+let level = document.cookie.split(";")[0].split("=")[1];
+let win = false;
+console.log(level);
+
 
 //Can't Play without Starting
 gameFrame.addEventListener("click", (e)=>{
@@ -18,12 +22,15 @@ gameFrame.addEventListener("click", (e)=>{
 //Starting Game
 startBtn.addEventListener("click", (e)=>{
     startGame();
+    document.getElementById("start-msg").classList.add("disappear");
 })
 
 //Starting Function
 function startGame(){
+    win = false;
     gameStarted = true;
     turn = "player";
+    printTurn("Player");
     //let player start playing
     for(let i = 0; i < 7 ; i++){
         //only allow playing if no win has been achieved
@@ -33,13 +40,13 @@ function startGame(){
 
 
 //Resetting all circles
-resetBtn.addEventListener("click", (e)=>{
-    //Ask the user if he/she wants to really reset the game coz they may press it by mistake
-    let answer = confirm("Are You Sure You Want to Reset the Game?");
-    if(answer){
-      resetGame();
-    }
-})
+// resetBtn.addEventListener("click", (e)=>{
+//     //Ask the user if he/she wants to really reset the game coz they may press it by mistake
+//     let answer = confirm("Are You Sure You Want to Reset the Game?");
+//     if(answer){
+//       resetGame();
+//     }
+// })
 
 //Resetting the Game
 function resetGame(){
@@ -76,12 +83,20 @@ function playerMoves(e){
             }
             allCircles[emptyPosition].classList.add("circle-yellow");
             allCircles[emptyPosition].removeEventListener("click", playerMoves)
+            printTurn("Computer");
             //Now we set the turn to be the computer's
             turn = "computer";
             //Check for winning
             //Set some time out so that the final move appears before the win window pops up
             setTimeout(() => {
-                checkForWinning("circle-yellow");
+                //check for draw anyways
+                checkDraw();
+                //If easy mode
+                if(level === "easy"){
+                    checkForWinningEasy("circle-yellow");
+                }else if(level === "hard"){
+                    checkForWinningHard("circle-yellow");
+                }
             }, 200);
             
             //If no win, let the computer play its move
@@ -122,18 +137,26 @@ function computerMoves(){
             }
         }
         allCircles[empty].classList.add("circle-red");
+        printTurn("Player");
         //Set some time out so that the final move appears before the win window pops up
         setTimeout(() => {
-            checkForWinning("circle-red");
+            //check for draw anyways
+            checkDraw();
+            //if easy level
+            if(level === "easy"){
+                checkForWinningEasy("circle-red");
+            }else if(level === "hard"){
+                checkForWinningHard("circle-red");
+            }
         }, 200);
         turn = "player";
     }
 }
 
 
-//Checking for winning and losing 
+//Checking for winning and losing Easy Mode
 
-function checkForWinning(winnerClass){
+function checkForWinningEasy(winnerClass){
     //Horizontal
     let winCounts = 0;
     for(let i = allCircles.length - 1; i >= 0; i -= 7){
@@ -155,7 +178,7 @@ function checkForWinning(winnerClass){
     if(winCounts === 4){
         console.log(winnerClass + ": " + winCounts);
         //Announce win and stop playing 
-        announceWin(winCounts, winnerClass);
+        announceWinEasy(winCounts, winnerClass);
         break;
     }
 }
@@ -181,15 +204,15 @@ function checkForWinning(winnerClass){
         if(winCounts === 4){
             console.log(winnerClass + ": " + winCounts);
             //Announce win and stop playing 
-            announceWin(winCounts, winnerClass);
+            announceWinEasy(winCounts, winnerClass);
             break;
         }
     }  
 }
 
 
-//Announce winning function
-function announceWin(winCounts, winnerClass){
+//Announce winning function for Easy Mode
+function announceWinEasy(winCounts, winnerClass){
     //Win if 4 in a row or more
     if(winCounts >= 4 && winnerClass === "circle-yellow"){
         let response = confirm("You Win! Would You Like To Play Again?");
@@ -205,8 +228,173 @@ function announceWin(winCounts, winnerClass){
         stopGame();
         //change turn to over
         turn = "over";
+        win = true;
+    }else{
+        let response = alert("Draw");
+        replayOrNot(response);
+        //stop playing
+        stopGame();
+        //change turn to over
+        turn = "over";
+        win = true;
+    }  
+}
+
+
+
+//Checking for winning and losing Hard Mode
+function checkForWinningHard(winnerClass){
+    //Horizontal
+    let winCounts = 0;
+    for(let i = allCircles.length - 1; i >= 0; i -= 7){
+        winCounts = 0;
+    for(let j = i; j > i - 7; j--){
+        if(allCircles[j].classList.contains(winnerClass)){
+            winCounts += 1;
+        }else{
+            winCounts = 0;
+        }
+
+        //break loop if finds win
+        if(winCounts === 5){
+            break;
+        }
+        
+    }
+    //break outer loop in case of a victory
+    if(winCounts === 5){
+        console.log(winnerClass + ": " + winCounts);
+        //Announce win and stop playing 
+        announceWinHard(winCounts, winnerClass);
+        break;
+    }
+}
+
+
+    //Vertical
+    for(let i = allCircles.length - 1; i >= 35; i--){
+        winCounts = 0;
+        for(let j = i; j >= 0; j -= 7){
+            if(allCircles[j].classList.contains(winnerClass)){
+                winCounts += 1;
+            }else{
+                winCounts = 0;
+            }
+
+            //break loop if finds win
+            if(winCounts === 5){
+                break;
+            }
+            
+        }
+        //break outer loop in case of a victory
+        if(winCounts === 5){
+            console.log(winnerClass + ": " + winCounts);
+            //Announce win and stop playing 
+            announceWinHard(winCounts, winnerClass);
+            break;
+        }
+    }
+    
+    //bug with line 40
+    //Diagonal From the right
+    for(let i = allCircles.length -1; i >= 34; i--){
+        winCounts = 0;
+        if(i < 39 && i > 34){
+            continue;
+        }
+        for(let j = i; j >= 0; j -= 8){
+            if(allCircles[j].classList.contains(winnerClass)){
+                winCounts += 1;
+            }else{
+                winCounts = 0;
+            }
+
+            //break loop if finds win
+            if(winCounts === 5){
+                break;
+            }
+
+        }
+        //break outer loop in case of a victory
+        if(winCounts === 5){
+            console.log(winnerClass + ": " + winCounts);
+            //Announce win and stop playing 
+            announceWinHard(winCounts, winnerClass);
+            break;
+        }
+    }
+
+    //Diagonal From the left
+    for(let i = 37; i >= 28; i--){
+        winCounts = 0;
+        if(i < 35 && i > 28){
+            continue;
+        }
+        for(let j = i; j >= 0; j -= 6){
+            if(allCircles[j].classList.contains(winnerClass)){
+                winCounts += 1;
+            }else{
+                winCounts = 0;
+            }
+
+            //break loop if finds win
+            if(winCounts === 5){
+                break;
+            }
+
+        }
+        //break outer loop in case of a victory
+        if(winCounts === 5){
+            console.log(winnerClass + ": " + winCounts);
+            //Announce win and stop playing 
+            announceWinHard(winCounts, winnerClass);
+            break;
+        }
+    }
+}
+
+
+//Announce winning function for Hard Mode
+function announceWinHard(winCounts, winnerClass){
+    //Win if 4 in a row or more
+    if(winCounts >= 5 && winnerClass === "circle-yellow"){
+        let response = confirm("You Win! Would You Like To Play Again?");
+        replayOrNot(response);
+        //stop playing
+        stopGame();
+        //change turn to over
+        turn = "over";
+        win = true;
+    }else if(winCounts >= 5 && winnerClass === "circle-red"){
+        let response = alert("The Computer Wins! Would You Like To Play Again?");
+        replayOrNot(response);
+        //stop playing
+        stopGame();
+        //change turn to over
+        turn = "over";
+        win = true;
     }   
 }
+
+
+
+function checkDraw(){
+    let countOfMoves = 0;
+    for(let i = 0; i < allCircles.length; i++){
+        if(allCircles[i].classList.contains("circle-red") || allCircles[i].classList.contains("circle-yellow")){
+            countOfMoves += 1;
+        }
+    }
+    if(countOfMoves === allCircles.length && win === false){
+        setTimeout(() => {
+         answer = confirm("It's a Draw! Would You Like to Replay?");
+         replayOrNot(answer);
+        }, 200); 
+    }
+}
+
+
 
 
 //Handling Replaying
@@ -225,4 +413,10 @@ function stopGame(){
         //Remove event listeners in case of a win
         allCircles[i].removeEventListener("click", playerMoves);
     }
+}
+
+//Printing whose turn it is on the screen
+function printTurn(turn) {
+    let turnLabel = document.getElementById("turn");
+    turnLabel.innerText = turn+"'s Turn";
 }
